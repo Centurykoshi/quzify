4d27e1920dc82fb0f00b131d9cadf71c5c5fbcf5
Piyush Yadav <koshijeager@gmail.com>
2025-09-19 22:44:28 +0530

    Update quiz.ts

--- src/schemas/forms/quiz.ts
+++ src/schemas/forms/quiz.ts
@@ -10,7 +10,7 @@ export const quizCreationSchema = z.object({
       message: "Topic must be at most 50 characters long",
     }),
   type: z.enum(["mcq", "open_ended"]),
-  amount: z.number().min(1).max(20), // Increased max for game creation
+  amount: z.number().min(1).max(50), // Increased max for game creation
 });
 
 export const questionGenerationSchema = z.object({
